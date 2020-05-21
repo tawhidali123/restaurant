04fa4350ed679f26978f83ed3489fc80757f9909
Tawhid <gem@tawhid.fios-router.home>
2020-05-20 20:08:27 -0400

    styled home

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,29 +2,50 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Container = styled.div`
-    display: grid;
+    .wrapper {
+        height: 100vh;
+        width: 100vw;
+        display: grid;
+    }
 
+    .title{
+        grid-column: 1 / 3;
+        grid-row: 2 / 3;
+        text-align: center; 
+    }
+
+    .buttons{
+        grid-column: 3 / 4;
+        grid-row: 2 / 3;
+    }
+
+    button{
+        height: 5vh;
+        width: 20vw;
+        margin: 1vh;
+    }
+    
+    
 `;
 
-const Title = styled.div`
-    grid-column-start: 2;
-    grid-column-end: 5;
-    grid-row-start: 1;
-    grid-row-end: 3; 
-`
+
 
 export default function Home(props) {
     return (
         <Container>
-            <Title>
-                <h1>Restaurant name</h1>
-            </Title>
+            <div className='wrapper'>
+                <div className='title'>
+                    <h1>Restaurant name</h1>
+                </div>
 
-            <div className='buttons'>
-                <button>Menu</button>
-                <button>Order Online</button>
-                <button>Phone Number</button>
-            </div>
+                <div className='buttons'>
+                    <button>Menu</button>
+                    <br />
+                    <button>Order Online</button>
+                    <br />
+                    <button>Phone Number</button>
+                </div>
+            </div>  
         </Container>
     )
 }
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,19 +1,26 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+
+`
 
 export default function Menu(props) {
     return (
-        <div>
+        <Container>
             <div>
-                <h2>Menu</h2>
-            </div>
+                <div>
+                    <h2>Menu</h2>
+                </div>
 
-            <div className='menuList'>
-                <div className='menuItem'>
-                    <h4>chicken</h4>
-                    <p>description</p>
-                    <img  className='image' src='https://40aprons.com/wp-content/uploads/2019/12/paleo-gluten-free-chicken-piccata-9.jpg' />
+                <div className='menuList'>
+                    <div className='menuItem'>
+                        <h4>chicken</h4>
+                        <p>description</p>
+                        <img  className='image' src='https://40aprons.com/wp-content/uploads/2019/12/paleo-gluten-free-chicken-piccata-9.jpg' />
+                    </div>
                 </div>
             </div>
-        </div>
+        </Container>
     )
 }
