048da3c4f81eb3870d0bb4d7a91d6bd88bfab1e5
Tawhid <gem@tawhid.fios-router.home>
2020-06-06 22:42:58 -0400

    react router applied

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import {Route, Switch, Link} from 'react-router-dom';
+
 import Home from './components/Home.js'
 import Menu from './components/Menu.js'
+
 
 function App() {
   return (
     <div>
-      <Home />
-      <Menu />
+      <Switch>
+        <Route path="/home" render={(routerProps) => <Home routerProps={routerProps}/>}/>
+        <Menu />
+      </Switch>
     </div>
   );
 }
