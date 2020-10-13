bc189661bdd082daa9b64003eeda9b3c8bfc04b2
Tawhid <gem@tawhid.fios-router.home>
2020-10-13 13:11:14 -0400

    fixed image sizing

--- src/App.js
+++ src/App.js
@@ -6,14 +6,31 @@ const menu =
 {
   Appetizers: 
   [
-    
+    {
+      name: 'GARLIC MASHED POTATOES',
+      priceInDollars: 16,
+      description: 'The taste of comfort is given Southern Brazilian flair with roasted garlic, drawn butter, cracked pepper, and fresh chives.',
+      photoURL: 'https://i2.wp.com/grandbaby-cakes.com/wp-content/uploads/2019/09/Garlic-Mashed-Potatoes-Recipe-1-500x375.jpg'
+    },
+    {
+      name: 'PÃO DE QUEIJO',
+      priceInDollars: 13,
+      description: 'Warm, soft cheesy bread rolls are served throughout the entire meal. Our irresistible homemade recipe from São Paulo can’t be missed. These rolls are naturally gluten-free.',
+      photoURL: 'https://i.pinimg.com/originals/cb/c4/06/cbc406ae3d5dcec7ebc30b05054afab7.jpg'
+    },
+    {
+      name: 'FAROFA',
+      priceInDollars: 22,
+      description: 'A staple of many meals in Brazil, farofa is yuca flour that is sautéed with bacon, sausage and light seasonings. Perfect complement to churrasco.',
+      photoURL: 'https://www.kitchenbutterfly.com/wp-content/uploads/2015/04/16948776979_aa62fc5eca_c.jpg'
+    }
   ],
   Entrees:
   [
     {
       name: 'Chicken Cacciatore',
       priceInDollars: 22,
-      description: 'a savory cut of roasted chicken covered in a decadent cream made from peas, spices, and pork fat',
+      description: 'a savory cut of roasted chicken covered in a decadent cream made from peas, spices, and olive oil',
       photoURL: 'https://40aprons.com/wp-content/uploads/2019/12/paleo-gluten-free-chicken-piccata-9.jpg'
     },
     {
@@ -25,17 +42,51 @@ const menu =
     {
       name: 'Garlic Pernil',
       priceInDollars: 32,
-      description: 'roasted pork that balances juicy on the inside with crispy on the outside, characterized by a light sweetness and strong garlic undertones',
+      description: 'roasted beef that balances juicy on the inside with crispy on the outside, characterized by a light sweetness and strong garlic undertones',
       photoURL: 'https://diversedinners.com/wp-content/uploads/2018/12/Pernil-6060.jpg'
     }
   ],
   Desserts:
   [
-
+    {
+      name: 'Trio Ice Cream',
+      priceInDollars: 13,
+      description: 'made with extra cream for an unforgettable flavor bite after bite',
+      photoURL: 'https://exhibitorsearch.messefrankfurt.com/images/large/userdata/bata/160529/5a675fb1d9535.jpg'
+    },
+    {
+      name: 'Vanilla Ice cream',
+      priceInDollars: 8,
+      description: 'made with extra cream for an unforgettable flavor bite after bite',
+      photoURL: 'https://static.toiimg.com/thumb/54677722.cms?imgsize=134423&width=800&height=800'
+    },
+    {
+      name: 'Chocolate Ice cream',
+      priceInDollars: 8,
+      description: 'made with extra cream for an unforgettable flavor bite after bite',
+      photoURL: 'https://bakingamoment.com/wp-content/uploads/2019/08/IMG_5627-chocolate-ice-cream.jpg'
+    }
   ],
   Drinks: 
   [
-
+    {
+      name: 'Pepsi',
+      priceInDollars: 4,
+      description: 'Cola Drink',
+      photoURL: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/pepsi-1568232214.jpg'
+    },
+    {
+      name: 'Sprite',
+      priceInDollars: 4,
+      description: 'Lemon-Lime Drink',
+      photoURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTWdVLa7CXs0M15dtTyHwm9sgVLbUMuYJi23Q&usqp=CAU'
+    },
+    {
+      name: 'Fanta',
+      priceInDollars: 4,
+      description: 'Orange Flavored Soda',
+      photoURL: 'https://brandstruck.co/wp-content/uploads/2017/01/fanta-logo.jpg'
+    }
   ]
 }
 
--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -7,11 +7,13 @@ function Showcase(props)
 
             <h2 style={{fontSize: '48px', fontWeight: '200', fontFamily: 'Kumbh Sans', marginTop: '54px', width: '65%', textAlign: 'left'}}>{ props.item.name }</h2>
             <div style={{height: '80%', display: 'flex', justifyContent: 'space-between', width: '65%'}}>
-            <img style={{maxHeight: '50vh', marginRight: '36px', maxWidth: '30vw'}} src={ props.item.photoURL } />
-            <div style={{width: '55%', display: 'flex', flexDirection: 'column'}}>
-            <h4 style={{fontSize: '30px', fontWeight: '300'}}>{ props.item.description }</h4>
-            <p style={{fontWeight: '200', fontSize: '24px'}}><i>{props.item.price ? `$${ props.item.price }` : null}</i></p>
-            </div>
+                <div style={{height: '100%', width: '100%'}}>
+                <img style={{maxHeight: '50vh', marginRight: '36px', maxWidth: '30vw'}} src={ props.item.photoURL } />
+                </div>
+                    <div style={{width: '55%', display: 'flex', flexDirection: 'column'}}>
+                        <h4 style={{fontSize: '30px', fontWeight: '300'}}>{ props.item.description }</h4>
+                        <p style={{fontWeight: '200', fontSize: '24px'}}><i>{props.item.price}</i></p>
+                    </div>
             </div>
 
       </div>
